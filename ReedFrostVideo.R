c07b24c8afa6db5667a69034720cf9717c37564c
Jamie Forschmiedt <73968036+jamie-forschmiedt@users.noreply.github.com>
2020-11-29 21:08:36 -0800

    Add files via upload

--- ReedFrostVideo.R
+++ ReedFrostVideo.R
@@ -9,12 +9,12 @@ library(animation)
 # This is the pathway on my computer.
 ani.options("ffmpeg" = "C:/Users/jamie/Downloads/ffmpeg-2020-11-18-git-e3081d6f4f-full_build/bin/ffmpeg")
 
-# Creates a video of a disease spreading through a population according to the Reed-Frost SIR Model
+# The rfvideo function creates a video of a disease spreading through a population according to the 
+# Reed-Frost Susceptible-Infected-Recovered (SIR) Model.
 # size = population size
 # I0 = initial number of infected individuals
 # p = probability of contact between each infected and susceptible person at each time step
 # note: the nodes' relative positions on the graph are meaningless and do not affect probability of contact
-## note to self: remove numI since it's not needed for the animation??
 rfvideo <- function(size, I0, p) {
   g1 <- graph(edges=c(), n=size, directed=F) #create a graph with size vertices, no edges
   Status <- c() #vector representing the status of each person in the population
@@ -25,7 +25,6 @@ rfvideo <- function(size, I0, p) {
     Status <- c(Status, "S")
   }
   V(g1)$status = Status # assign the elements in the "Status" vector to the nodes in the graph
-  #l <-layout.graphopt(g1) # set the layout of the graph to "graphopt"
   
   #begin the animation
   saveVideo({
@@ -38,24 +37,19 @@ rfvideo <- function(size, I0, p) {
          main=paste("Reed-Frost Simulation, population size =", size, ", p =", p))
     legend(1, 1, legend=c("S", "I", "R"), pch=19, col=c("green", "red", "gray"))
     
-    numI <- I0 #number of infections (initially I0)
     newStatus <- Status # vector for the status of the nodes at the new time step
     
     while ("I" %in% Status) { #while there are infected people in the population
-      newNumI <- 0 #new infections in this time step (initially zero)
-      
       for (i in 1:length(Status)) { #for each node in the population
         if (Status[i] == "I") { #if they are infected
           newStatus[i] <- "R" #set their new status to recovered
-          
           for (j in 1:length(Status)) { #traverse the population again
             if (Status[j] == "S") { #if they are susceptible
-              u <- runif(1) #create a U ~ Unif(0, 1) random variable
-              if (u < p) { #if U is less than the p specified in the parameters
+              u <- runif(1) #create a Uniform(0,1) random variable
+              if (u < p) { #if u is less than the p specified in the parameters
                 g1 <- g1 + edge(i,j) #draw an edge between the infected node and the susceptible node
                 if (newStatus[j] != "I") { #if node j hasn't already been infected this time step
                   newStatus[j] <- "I" #set its new status to infected
-                  newNumI <- newNumI + 1 #add 1 to the number of new infections this time step
                 }
               }
             }
@@ -63,7 +57,6 @@ rfvideo <- function(size, I0, p) {
         }
       }
       
-      numI <- newNumI #number of infections = new number of infections
       Status <- newStatus #set status to new status
       V(g1)$status <- Status #assign the new status to each node in the graph
       
@@ -77,3 +70,5 @@ rfvideo <- function(size, I0, p) {
   }, video.name="rfvideo.mp4", interval=1)
 }
 
+# Run the function to create the video
+rfvideo(150, 1, 0.03) #example: population size=150, initial infections=1, p=0.03
